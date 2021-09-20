2d0e0cc4c0caa26bef645da672977d6ed60d864b
HaraldPurnell <36031747+HaraldPurnell@users.noreply.github.com>
2021-09-20 12:17:09 +0100

    Update priceSorter.java

--- websiteScraper/src/websitescraper/priceSorter.java
+++ websiteScraper/src/websitescraper/priceSorter.java
@@ -1,18 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package websitescraper;
 
 import java.util.Comparator;
 
 /**
  *
- * @author Uglybear
+ * @author Harald Purnell
  */
 public class priceSorter implements Comparator<product> {
-
+    
+    /* Documentation for Comparator can be found at:
+    https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html */
+    
     @Override
     public int compare(product o1, product o2) {
         return o2.getFormattedPrice() - o1.getFormattedPrice();
